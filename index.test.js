7d729a346228c6e1912eb89f992a6c112265c045
byronPantoja <byronpantoja@gmail.com>
2020-04-21 07:57:14 +0800

    Refactoring test logic so that it works with the mocha library

--- index.test.js
+++ index.test.js
@@ -2,24 +2,24 @@ const assert = require('assert').strict;
 
 const { forEach, map } = require('./index');
 
-const test = (desc, fn) => {
-    console.log('----', desc);
-    try {
-        fn();
-    } catch (err) {
-        console.log(err.message);
-    }
-};
+// const test = (desc, fn) => {
+//     console.log('----', desc);
+//     try {
+//         fn();
+//     } catch (err) {
+//         console.log(err.message);
+//     }
+// };
 
-test('The forEach fn', () => {
+it('The forEach fn', () => {
     let sum = 0;
     forEach([1, 2, 3], value => {
         sum += value;
     });
-    assert.strictEqual(sum, 6, 'Expected forEach to sum the array');
+    assert.strictEqual(sum, 7, 'Expected forEach to sum the array');
 });
 
-test('The result fn', () => {
+it('The result fn', () => {
     const result = map([1, 2, 3], value => {
         return value * 2;
     });
